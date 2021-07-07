a44c4c659cd564eb0a241fddc8896ae4fc044676
HeewonKwak <81952911+HeewonKwak@users.noreply.github.com>
2021-07-07 20:51:10 +0900

    Python_django web :2021:07:07 commit 3

--- gys_project1/settings.py
+++ gys_project1/settings.py
@@ -34,7 +34,7 @@ while True:
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oh7sy)gqy3=v@@@r#_6a2mzix503s34-c#3@@mo4%^!d%my@$g'
+SECRET_KEY = env_list[key]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
