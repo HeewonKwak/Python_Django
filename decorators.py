ba6d4d80fbabbd920e67e98b3e756506370bd1fb
HeewonKwak <81952911+HeewonKwak@users.noreply.github.com>
2021-07-21 16:15:05 +0900

    Python_django web :2021:07:21 commit 4

--- decorators.py
+++ decorators.py
@@ -1,14 +1,30 @@
 
+# def decorator(func):
+#     def decorated(input_text):
+#         print('함수 시작!')
+#         func(input_text)
+#         print('함수 끝!')
+#     return  decorated
+#
+# @decorator
+# def hello_world(input_text):
+#     print(input_text)
+#
+#
+# hello_world('Hello World!')
+
 def decorator(func):
-    def decorated(input_text):
-        print('함수 시작!')
-        func(input_text)
-        print('함수 끝!')
-    return  decorated
+    def decorated(width, height):
+        if width >= 0 and height >= 0:
+            return func(width, height)
+        else:
+            raise ValueError('Input positive value!')
+    return decorated
 
 @decorator
-def hello_world(input_text):
-    print(input_text)
+def triangle_region(width, height):
+    return width*height*0.5
 
-
-hello_world('Hello World!')
+@decorator
+def square_region(width, height):
+    return width*height
